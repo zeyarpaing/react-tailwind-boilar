e843fe3428894fd034b532e9efe07f7cb1498037
Zeyar Paing <zeyarpaing@ucsy.edu.mm>
2020-12-06 11:51:03 +0630

    add: prettier formatter for beauty code

--- src/App.js
+++ src/App.js
@@ -1,27 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
-import { useState } from 'react';
-import Image from './components/Image';
-const arr = ['Hellow', "World", "Blah"];
+import logo from "./logo.svg";
+import "./App.css";
+import { useState } from "react";
+import Image from "./components/Image";
+const arr = ["Hellow", "World", "Blah"];
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
     <>
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>{count}</p>
-        <button onClick={(e) =>{
-          console.log(e)
-          setCount(count + 1)
-        }
-        }>Click to count</button>
-      </header>
-    </div>
-    {
-      arr.map((title, idx) => <Image title={title} key={idx} />)
-    }
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>{count}</p>
+          <button
+            onClick={(e) => {
+              console.log(e);
+              setCount(count + 1);
+            }}
+          >
+            Click to count
+          </button>
+        </header>
+      </div>
+      {arr.map((title, idx) => (
+        <Image title={title} key={idx} />
+      ))}
     </>
   );
 }
